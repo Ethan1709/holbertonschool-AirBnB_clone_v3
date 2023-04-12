30cba043fb2e3631fb9f225e57b9a58483113649
EXTRADEEN <dinumira@gmail.com>
2023-04-12 14:33:19 +0200

    PEP8 fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,19 +6,21 @@ from api.v1.views import app_views
 from models.state import State
 from models import storage
 from models import base_model
-import models
 
 
 @app_views('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    states = models.storage.all(State).values()
+    """ Retrieve the list of all State """
+    states = storage.all(State).values()
     states_list = []
     for state in states:
         states_list.append(state.to_dict())
     return jsonify(states_list)
 
+
 @app_views('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
+    """ If the state_id is not linked to any State object, raise a 404 error """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
