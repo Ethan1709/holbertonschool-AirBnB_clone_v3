a50c6fc415e62271d2c19e957da7ff2fdccbc0de
EXTRADEEN <dinumira@gmail.com>
2023-04-12 11:39:54 +0200

    PEP8 validation fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,9 +17,11 @@ def app_tear(exception):
     """ call storage.close """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
